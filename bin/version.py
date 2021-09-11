54a991759240553b66cb8620650a3d100efd92e4
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-09-10 20:33:42 -0700

    Add version.py

---
+++ bin/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import torch
+print(f"torch.version={torch.__version__}")
+
+import pytorch_lightning
+print(f"pytorch_lightning.version={pytorch_lightning.__version__}")
+
+import transformers 
+print(f"transformers.version={transformers.__version__}")
+
