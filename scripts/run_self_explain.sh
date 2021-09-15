151aa8032d89f33041948926706460cb75feb0b1
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-09-14 17:01:28 -0700

    Clean up train.py

--- scripts/run_self_explain.sh
+++ scripts/run_self_explain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TOKENIZERS_PARALLELISM=false
-echo python bin/run.py --dataset_basedir data/SST-2-XLNet \
+echo python bin/train.py --dataset_basedir data/SST-2-XLNet \
                          --lr 2e-5  --max_epochs 5 \
                          --gpus 1 \
                          --concept_store data/SST-2-XLNet/concept_store.pt \
