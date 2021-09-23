e05e4e0109eaa5ce50096bc520f8e3a79920416a
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-09-23 10:51:29 -0700

    Development version 0.0.3dev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     requirements = f.read()
 
 setup(name='self_explain',
-      version='0.0.2',
+      version='0.0.3dev',
       description='Self Explain module',
       url='http://github.com/asundaresan/SelfExplain.git',
       author='Dheeraj Rajagopal, Vidisha Balachandran, Artidoro Pagnoni, Aravind Sundaresan',
