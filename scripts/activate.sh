b45b52a99489846379f46364c2ba62f5d7a2d77c
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-09-28 09:55:24 -0700

    Update venv scripts

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,21 +1,15 @@
 #!/bin/bash 
 
-version=`python3 -c "import sys; version = '.'.join( map( str, sys.version_info[:2] ) ); print( version )"`
-folder=venv/${HOSTNAME}_${version}
-if [ $# -gt 0 ]; then 
-  if [ -d $1 ]; then 
-    echo "Using virtualenv ${1} instead of default ${folder}"
-    folder=$1
-  else
-    echo "Folder does not exist: ${1}"
-  fi
-fi
+# get default virtualenv folder name
+version=`python3 -c "import platform; print(platform.python_version())"`
+folder=venv/`hostname -s`_${version}
 
+# if virtualenv is not installed, install and update it
 if [[ ! -f ${folder}/bin/activate ]]; then
   python3 -m venv ${folder}
+  echo "Please run the following to update to latest: "
+  echo python -m pip install -U pip setuptools wheel
 fi
 
 source ${folder}/bin/activate
-echo "Please run the following to update to latest: "
-echo python -m pip install -U pip setuptools wheel
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-# from inside the virtual environment 
 
-#python -m pip install -U pip setuptools wheel
+# this command should be run inside the virtual environment 
+# install this package in editable mode
 python -m pip install -e .
 
