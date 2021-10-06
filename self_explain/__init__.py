b62d09219ab5e3b14dabd8674ef7dc3e770af067
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-10-06 10:18:49 -0700

    Set resource and download benepar on import

--- self_explain/__init__.py
+++ self_explain/__init__.py
@@ -9,3 +9,22 @@ import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 from .self_explain import SelfExplainCharacterizer
 
+def download_benepar():
+    """ Download benepar data to ~/nltk_data
+    """
+    import benepar
+    benepar.download('benepar_en3')
+
+
+def set_resource_limit():
+    """ Required by self-explain
+    """
+    import resource
+    rlimit_old = resource.getrlimit(resource.RLIMIT_NOFILE)
+    rlimit_new = (4096, rlimit_old[1])
+    print(f"setting resource.RLIMIT_NOFILE={rlimit_new} (was {rlimit_old})")
+    resource.setrlimit(resource.RLIMIT_NOFILE, rlimit_new)
+
+set_resource_limit()
+download_benepar()
+
