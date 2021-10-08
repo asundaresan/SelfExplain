8f06d96006af449e12ef5a1c2505a285bcfc1e2b
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-10-08 15:30:06 -0700

    Update and comment building concept store to split sentences

--- bin/build_concept_store.py
+++ bin/build_concept_store.py
@@ -1,6 +1,6 @@
 import os 
 import argparse
-import self_explain 
+import logging
 from self_explain.preprocessing.build_concept_store import concept_store 
 
 def main():
@@ -18,8 +18,12 @@ def main():
 
     parser.add_argument("--max_concept_len", "-l", default=5, type=int, required=True,
                         help="Max length of concept")
+    parser.add_argument("--verbosity", "-v", action="count", default=0, help="Verbosity level")
 
     args = parser.parse_args()
+
+    console_level = logging.WARN if args.verbosity == 0 else logging.INFO if args.verbosity == 1 else logging.DEBUG
+    logging.basicConfig(level=console_level, format='[%(asctime)s %(levelname)s] %(message)s')
 
     concept_store(input_file_name=args.input_train_file,
                   output_folder=args.output_folder,
--- self_explain/preprocessing/build_concept_store.py
+++ self_explain/preprocessing/build_concept_store.py
@@ -1,7 +1,6 @@
-import argparse
+import logging
 import json
-from collections import OrderedDict
-import spacy
+from collections import OrderedDict, Counter
 
 import tqdm
 import spacy
@@ -30,23 +29,28 @@ def concept_store(model_name, input_file_name, output_folder, max_concept_length
     total = 0
     with open(input_file_name, 'r') as input_file:
         total = sum(1 for line in input_file)
-    print(f"found {total} lines in {input_file}")
+    print(f"found {total} lines in {input_file_name}")
 
-
+    logging.info(f"loading input from {input_file_name}")
+    sentence_lengths = Counter()
     with open(input_file_name, 'r') as input_file:
-        for i, line in enumerate(tqdm(input_file, total=total, desc="loading concepts")):
+        for i, line in enumerate(tqdm.tqdm(input_file, total=total, desc="loading concepts")):
             json_line = json.loads(line)
             # sentence is actually the full text
             text = json_line["sentence"].strip().strip(' .')
             doc = nlp(text)
             for sentence in doc.sents:
+                sentence = sentence.text
+                sentence_length = len(sentence.split())
+                sentence_lengths.update([sentence_length,])
+
                 if len(sentence.split()) <= max_concept_length:
                     concept_idx[idx] = sentence
                     idx += 1
 
     num_batches = len(concept_idx)//batch_size
     concept_tensor = []
-    for batch in tqdm.tqdm(chunks(list(concept_idx.values()), n=batch_size), total=num_batches):
+    for batch in tqdm.tqdm(chunks(list(concept_idx.values(), desc="building concepts"), n=batch_size), total=num_batches):
         inputs = tokenizer(batch, padding=True, return_tensors="pt")
         for key, value in inputs.items():
             inputs[key] = value.to('cuda')
@@ -54,6 +58,11 @@ def concept_store(model_name, input_file_name, output_folder, max_concept_length
         pooled_rep = sequence_summary(outputs[0])
         concept_tensor.append(pooled_rep.detach().cpu())
 
+    if False:
+        print(f"saving pickle")
+        import pickle
+        with open("concept_tensor.pickle", "wb") as handle:
+            pickle.dump(concept_store, handle)
     concept_tensor = torch.cat(concept_tensor, dim=0)
 
     torch.save(concept_tensor, f'{output_folder}/concept_store.pt')
@@ -62,30 +71,3 @@ def concept_store(model_name, input_file_name, output_folder, max_concept_length
 
     return
 
-
-def main():
-    parser = argparse.ArgumentParser()
-
-    ## Required parameters
-    parser.add_argument("--input_train_file", "-i", default=None, type=str, required=True,
-                        help="The input train file")
-
-    parser.add_argument("--output_folder", "-o", default='roberta-base', type=str, required=True,
-                        help="Output folder for concept store and dict")
-
-    parser.add_argument("--model_name", "-m", default='roberta-base', type=str, required=True,
-                        help="Model name")
-
-    parser.add_argument("--max_concept_len", "-l", default=5, type=int, required=True,
-                        help="Max length of concept")
-
-    args = parser.parse_args()
-
-    concept_store(input_file_name=args.input_train_file,
-                  output_folder=args.output_folder,
-                  model_name=args.model_name,
-                  max_concept_length=args.max_concept_len)
-
-
-if __name__ == "__main__":
-    main()
