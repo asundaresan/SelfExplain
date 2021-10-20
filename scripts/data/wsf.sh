142a36bbef1d83ada958d04ba81c71414da83c41
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-10-20 14:04:36 -0700

    Update scripts with new data location

--- scripts/data/wsf.sh
+++ scripts/data/wsf.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 
-export DATA_FOLDER="/export/home/`hostname -s`1/data/semafor/SE/wsf/balanced"
+export EXPERIMENT="wsf"
+export DATA_FOLDER="/export/home/`hostname -s`1/`whoami`/data/semafor/SE/${EXPERIMENT}/balanced"
 export TOKENIZER_NAME='xlnet-base-cased'
 export MAX_LENGTH=10
+
+echo export CUDA_VISIBLE_DEVICES=1
 
 # Creates jsonl files for train and dev
 echo python bin/store_parse_trees.py --data_dir $DATA_FOLDER  --tokenizer_name $TOKENIZER_NAME
@@ -11,7 +14,7 @@ echo python bin/store_parse_trees.py --data_dir $DATA_FOLDER  --tokenizer_name $
 # Since SST-2 already provides parsed output, easier to do it this way, for other datasets, need to adapt
 echo python bin/build_concept_store.py -i $DATA_FOLDER/train_with_parse.json -o $DATA_FOLDER -m $TOKENIZER_NAME -l $MAX_LENGTH
 
-echo python bin/train.py --dataset_basedir ${DATA_FOLDER} --lr 2e-5  --max_epochs 5 --gpus 1 --concept_store ${DATA_FOLDER}/concept_store.pt 
+echo python bin/train.py --dataset_basedir ${DATA_FOLDER} --lr 2e-5  --max_epochs 5 --gpus 1 --concept_store ${DATA_FOLDER}/concept_store.pt --default_root_dir logs/${EXPERIMENT}
 
 echo "--"
 echo python bin/infer_model.py --concept_map ${DATA_FOLDER}/concept_idx.json \
