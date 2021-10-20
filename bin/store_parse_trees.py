e54519de27cad867b2066eb85848761117aba0a9
Aravind Sundaresan <aravind.sundaresan@sri.com>
2021-10-20 14:47:47 -0700

    Update parse trees to add test and not overwrite by default

--- bin/store_parse_trees.py
+++ bin/store_parse_trees.py
@@ -9,11 +9,11 @@ def main():
     ## Required parameters
     parser.add_argument("--data_dir", default=None, type=str, required=True,
                         help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
-    parser.add_argument("--splits", default=["train", "dev"], type=str, nargs="+",
+    parser.add_argument("--splits", default=["train", "dev", "test"], type=str, nargs="+",
                         help="Which splits to process")
     parser.add_argument("--tokenizer_name", default='roberta-base', type=str, required=True,
                         help="Tokenizer name")
-
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite even if present")
     args = parser.parse_args()
 
     parsed_data = ParsedDataset(tokenizer_name=args.tokenizer_name, progress_bar=True)
@@ -21,10 +21,16 @@ def main():
     # Read input files from folder
     for file_split in args.splits:
         input_file_name = os.path.join(args.data_dir, file_split + '.tsv')
+        if not os.path.exists(input_file_name):
+            print(f"input file '{input_file_name}' not present, skipping")
+            continue
         output_file_name = os.path.join(args.data_dir, file_split + '_with_parse.json')
-        print(f"storing parsed trees {input_file_name} -> {output_file_name}")
-        parsed_data.read_and_store_from_tsv(input_file_name=input_file_name,
-                                            output_file_name=output_file_name)
+        if not os.path.exists(output_file_name) or args.overwrite:
+            print(f"storing parsed trees {input_file_name} -> {output_file_name}")
+            parsed_data.read_and_store_from_tsv(input_file_name=input_file_name,
+                                                output_file_name=output_file_name)
+        else:
+            print(f"output file '{output_file_name}' present, skipping. Use '--overwrite' option.")
 
 
 if __name__ == "__main__": 
